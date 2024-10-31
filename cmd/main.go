bb0bf3f9968002551e8c6ae7d96124746340e308
boriskudinov <kudinovb@gmail.com>
2024-10-31 18:18:04 +0300

    added database docker

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 	"log"
 	"moiPerviiProject"
 	"moiPerviiProject/pkg/handler"
+	"moiPerviiProject/pkg/repository"
+	"moiPerviiProject/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     "localhost",
+		Port:     "5436",
+		Username: "postgres",
+		Password: "qwerty",
+		DBName:   "postgres",
+		SSLMode:  "disable",
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(moiPerviiProject.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
